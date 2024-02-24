9df64013611eb52e00d475b4e1ce0a820767b55f
Shubham <Shubham@Shubham>
2024-02-24 22:21:44 +0530

    New add method class is added in project

---
+++ Hybrid_FrameWork/src/test/java/commonFunctions/DemoForNewFileAdd.java
@@ -0,0 +1,20 @@
+package commonFunctions;
+
+public class DemoForNewFileAdd 
+{
+	public void add()
+	{
+		int a=34,b=17;
+		int c=a+b;
+		System.out.println(c);
+	}
+	public void sub()
+	{
+		int a=34,b=17;
+		int c=a-b;
+		System.out.println(c);
+	}
+	
+	
+
+}
