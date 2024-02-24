de8bc2313c54c5ed4bf09bfddbdefd0de263ba29
Shubham <Shubham@Shubham>
2024-02-24 22:27:36 +0530

    Values modified and new method added

--- Hybrid_FrameWork/src/test/java/commonFunctions/DemoForNewFileAdd.java
+++ Hybrid_FrameWork/src/test/java/commonFunctions/DemoForNewFileAdd.java
@@ -4,14 +4,20 @@ public class DemoForNewFileAdd
 {
 	public void add()
 	{
-		int a=34,b=17;
+		int a=567,b=175;
 		int c=a+b;
 		System.out.println(c);
 	}
 	public void sub()
 	{
-		int a=34,b=17;
+		int a=340,b=100;
 		int c=a-b;
+		System.out.println(c);
+	}
+	public void mul()
+	{
+		int a=34,b=17;
+		int c=a*b;
 		System.out.println(c);
 	}
 	
